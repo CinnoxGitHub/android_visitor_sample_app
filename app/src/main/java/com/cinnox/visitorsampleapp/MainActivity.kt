2ef791dd27fa445e82c4e95f20fbf928ae3ad147
liuming <liuming@maaii.com>
2023-09-04 15:32:38 +0800

    [TAND-8673] implement the test app for verify the feature

--- app/src/main/java/com/cinnox/visitorsampleapp/MainActivity.kt
+++ app/src/main/java/com/cinnox/visitorsampleapp/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.cinnox.visitorsampleapp
 
-import android.content.ComponentName
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.cinnox.visitorsampleapp.push.genFcmIntentPushData
@@ -16,29 +16,21 @@ class MainActivity : AppCompatActivity() {
 
     companion object{
         const val TAG = "MainActivity"
+        private const val KEY_ACTION = "action"
+        private const val ACTION_LOGIN = "login"
+        private const val ACTION_LOGOUT = "logout"
+        private const val KEY_STAFF_ID = "StaffId"
+        private const val KEY_STATION_CODE = "StationCode"
+        private const val KEY_DISPLAY_NAME = "DisplayName"
     }
-
-    private lateinit var ssoTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         handleClickedSystemNotification()
-        ssoTextView = findViewById(R.id.tv_sso)
-        ssoTextView.setOnClickListener {
-            val intent = Intent().apply {
-                component = ComponentName(
-                    "com.m800.liveconnect.mobile.agent.tb.hkmtr", // this is Cinnox app package name
-                    "com.m800.liveconnect.mobile.agent.ui.MainActivity" // this is Cinnox app activity which used for login
-                )
-            }
-//            val intent = packageManager.getLaunchIntentForPackage("com.m800.liveconnect.mobile.agent.tb.hkmtr")!!
-            intent.putExtra("action", "login")
-            intent.putExtra("StaffId", "userStaffId")
-            intent.putExtra("StationCode", "userStationCode")
-            intent.putExtra("DisplayName", "userDisplayName")
-            startActivity(intent)
-        }
+        initLoginFlow()
+        initLogoutFlow()
+        initIncomingCallFlow()
     }
 
     override fun onNewIntent(intent: Intent?) {
@@ -65,5 +57,60 @@ class MainActivity : AppCompatActivity() {
         } ?: run {
             Log.i(TAG, "handleClickedSystemNotification data is null")
         }
+    }
+
+    private fun initLoginFlow() {
+        findViewById<TextView>(R.id.tv_login).setOnClickListener {
+            genIntent().apply {
+                putExtra(KEY_ACTION, ACTION_LOGIN)
+                putExtra(KEY_STAFF_ID, getStaffId())
+                putExtra(KEY_STATION_CODE, getStationCode())
+                putExtra(KEY_DISPLAY_NAME, getDisplayName())
+            }.run {
+                startActivity(this)
+            }
+        }
+    }
+
+    private fun initLogoutFlow() {
+        findViewById<TextView>(R.id.tv_logout).setOnClickListener {
+            genIntent().apply {
+                putExtra(KEY_ACTION, ACTION_LOGOUT)
+                putExtra(KEY_STAFF_ID, getStaffId())
+            }.run {
+                startActivity(this)
+            }
+        }
+    }
+
+    private fun initIncomingCallFlow() {
+        findViewById<TextView>(R.id.tv_incoming_call).setOnClickListener {
+            genIntent().run {
+                startActivity(this)
+            }
+        }
+    }
+
+    private fun genIntent(): Intent {
+//        val intent = Intent().apply {
+//            component = ComponentName(
+//                "com.m800.liveconnect.mobile.agent.tb.hkmtr", // this is Cinnox app package name
+//                "com.m800.liveconnect.mobile.agent.ui.MainActivity" // this is Cinnox app activity which used for login
+//            )
+//        }
+        val intent = packageManager.getLaunchIntentForPackage("com.m800.liveconnect.mobile.agent.tb.hkmtr")!!
+        return intent
+    }
+
+    private fun getStaffId(): String {
+        return findViewById<EditText>(R.id.et_staff_id).text.toString()
+    }
+
+    private fun getStationCode(): String {
+        return findViewById<EditText>(R.id.et_station_code).text.toString()
+    }
+
+    private fun getDisplayName(): String {
+        return findViewById<EditText>(R.id.et_display_name).text.toString()
     }
 }
